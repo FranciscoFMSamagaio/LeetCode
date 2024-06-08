754094d7efed2a6f7f9e5c5e923a1bc057fcd82a
Francisco Samagaio <157983243+FranciscoFMSamagaio@users.noreply.github.com>
2024-06-08 15:53:20 +0100

    Time: 424 ms (99.64%), Space: 0B (100%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,20 @@
+SELECT
+    to_char(trans_date, 'YYYY-MM') AS month,
+    country,
+    COUNT(distinct id) AS trans_count,
+    COUNT(distinct 
+        CASE
+            WHEN state = 'approved' THEN id 
+            ELSE null 
+            END
+        ) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(
+        CASE
+            WHEN state = 'approved' THEN amount 
+            ELSE 0 
+            END
+        )AS approved_total_amount
+FROM Transactions
+GROUP BY 1,2
+ORDER BY 1
